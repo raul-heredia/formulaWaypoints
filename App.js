a8f9800039485ba8ccf16faba92ffd1c48f51c3a
Raúl Heredia <raul.heredia01@outlook.com>
2022-04-09 13:01:12 +0200

    Tab navigator basico sin iconos

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from "@expo/vector-icons";
+
+// Componentes
+import { Map } from './app/views/Map';
+import { Llista } from './app/views/Llista';
+
+
+const Tab = createBottomTabNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Mapa" component={Map} />
+        <Tab.Screen name="Llista" component={Llista} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
